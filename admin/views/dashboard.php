4af9c6d3abb2c31b164b17e5717f9be2d07875d1
Mariana-reynaga <mariana.reynaga@davinci.edu.ar>
2024-07-08 13:49:17 -0300

    cambios al navbar para que las secciones sean un menu desplegable

--- admin/views/dashboard.php
+++ admin/views/dashboard.php
@@ -1,4 +1,6 @@
 <?php
-    echo "este es el dashboard";
+    // echo "este es el dashboard";
 
 ?>
+
+<h1 class="text-center my-4">Dashboard</h1>
--- componente/nav.php
+++ componente/nav.php
@@ -15,25 +15,31 @@
 ?>
 
 <div class="container-fluid" style="background-color: #CBF3F0;">
-    <header class="d-flex flex-wrap justify-content-center py-3 border-bottom">
+    <header class="d-flex flex-wrap justify-content-center align-items-center py-3 border-bottom">
         <a href="index.php?sec=home" class="d-flex align-items-center mb-3 mb-md-0 me-md-auto link-body-emphasis text-decoration-none">
             <span class="fs-4 fw-bold" style="color: #2B2D42">M Point</span>
         </a>
 
-        <ul class="nav nav-pills">
-            <li class="nav-item"><a href="index.php?sec=todosManga" class="nav-link">Manga</a></li>
+        <div class="dropdown">
+                <a class="btn dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown">
+                    Secciones
+                </a>
 
-            <?php foreach ($secciones as $genero) { ?>
+                <ul class="dropdown-menu">
+                    <?php foreach ($secciones as $genero) { ?>
                         <li class="nav-item">
-                            <a href="index.php?sec=mangaXcat&gen=<?=$genero->getID()?>" class="nav-link">
+                            <a href="index.php?sec=mangaXcat&gen=<?=$genero->getID()?>" class="dropdown-item">
                                 <?= $genero->getGenero() ?>
                             </a>
                         </li>
-            <?php } ?>
-            
+                    <?php } ?>
+                </ul>
+        </div>
+
+        <ul class="nav nav-pills">
+            <li class="nav-item"><a href="index.php?sec=todosManga" class="nav-link">Manga</a></li>
             <li class="nav-item"><a href="index.php?sec=contacto" class="nav-link">Contacto</a></li>
             <li class="nav-item"><a href="index.php?sec=datosAlum" class="nav-link">Datos</a></li>
-
         </ul>
     </header>
 </div>
