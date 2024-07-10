f96f179c0cd4a00afc8b173cc18e2860a4daacd9
Mariana-reynaga <mariana.reynaga@davinci.edu.ar>
2024-07-10 15:45:21 -0300

    cambios al cambio de portada para que no se elimine en caso de que no se suba una portada nueva

--- admin/acciones/editarManga.php
+++ admin/acciones/editarManga.php
@@ -10,10 +10,15 @@
     echo "</pre>";
 
     $imgNueva = $_FILES["portada"] ?? FALSE; //Traigo la portada nueva, en caso de que este vacio me devuelve un falso
-
+    
     try {
         if ($imgNueva) { //si tengo una imagen nueva
             
+            
+
+        };
+
+        if (!($_FILES["portada"]["error"] == 4)) { //cambios si se subio una nueva portada
             (new Portada())->deleteFile("../../img/portadas/".$_POST["portada-og"]); //elimino el archivo de la carpeta portadas
             
             $newName = (new Portada())->subirImagen($imgNueva, "../../img/portadas"); //subo la imagen nueva a la carpeta portadas
@@ -24,10 +29,12 @@
                 $_POST["txtAlt"]
             );
 
-        };
-        
+        }else{ //cambios si no se subio una nueva portada
+            echo "no hay nada";
+        }
+
         header("Location: ../index.php?sec=home");
-    } catch (\Throwable $th) {
-        //throw $th;
+    } catch (Exception $e) {
+        echo $e->getMessage();
     }
 ?>
